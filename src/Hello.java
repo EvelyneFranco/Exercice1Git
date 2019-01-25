b2ccd3bac4c978635ade0ff07ce600e323eee9a8
Evelyne Franco <evelyne.franco@trustvesta.com>
2019-01-24 23:38:52 -0600

    commit project

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+	
+	public static void main (String args[]) {
+		System.out.println("This method is added from Add branch");
+		System.out.println("will be merge to master branch");
+		
+	}
+
+}
